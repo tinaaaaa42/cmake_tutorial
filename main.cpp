d0691dde7573c387858fc3e7dfdf8b8ee1fcb70f
tinaaaaa42 <805210884@qq.com>
2023-05-04 13:24:22 +0800

    The main file of get_answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "answer.hpp"
+
+int main(int agrc, const char* argv[]) {
+    int expected_answer = answer::expected_answer();
+    for (;;) {
+        std::cout << "What is the ultimate answer?" << std::endl;
+        int answer;
+        std::cin >> answer;
+        if (answer == expected_answer) {
+            std::cout << "Correct!" << std::endl;
+            break;
+        } else {
+            std::cout << "Wrong!" << std::endl;
+        }
+    }
     return 0;
 }
