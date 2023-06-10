3189cce22b7bf6325c39c3836513edf7857879a8
tinaaaaa42 <805210884@qq.com>
2023-06-10 18:08:38 +0800

    Change API ID to Cathe string

--- answer/answer.cpp
+++ answer/answer.cpp
@@ -12,7 +12,7 @@ namespace answer {
     namespace v2 {
         std::string expected_answer() {
             // use curl to get the answer from the internet
-            return wolfram::simple_query("WGX8JG-KE8XGJW6R7", "What is the ultimate answer?");
+            return wolfram::simple_query(WOLFRAM_APIID, "What is the ultimate answer?");
         }
     }
 }
